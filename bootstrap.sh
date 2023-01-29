189e3c8baa75bfc37fbab475f229e71dd7349e95
z3mil <72978258+z3mil@users.noreply.github.com>
2023-01-29 18:04:46 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+#
 # Environments
-SHELLRC="/etc/profile"
-if [[ "$SHELL" -eq "/bin/bash" ]]; then
-  SHELLRC="$HOME/.bashrc"
-fi
-# Default PS1 variable value
+#
+SHELLRC="$HOME/.bashrc"
 PS1="\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1;31m\]\u\[\e[1;36m\]\[\033[m\]@\[\e[1;36m\]\h\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\W]> \[\e[0m\]"
 
 # Help function
