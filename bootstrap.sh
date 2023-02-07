6c438e45cce1886a67ebc1b5335c2cd12c75dd93
z3mil <72978258+z3mil@users.noreply.github.com>
2023-02-07 18:46:08 +1000

    Added the corrrect perms on authorized_keys

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,6 +48,7 @@ alias l="ls -lah"
 
 # set Pub Key Access to user
 if [ ! -d $HOME/.ssh ]; then
-  mkdir -p $HOME/.ssh; chmod 600 $HOME/.ssh;
+  mkdir -p $HOME/.ssh; chmod 700 $HOME/.ssh;
 fi
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVvVrgF7abC0Bk8KIeNLfTT+wGvHPodJkt0YkS04eNF" >> $HOME/.ssh/authorized_keys
+chmod 600 $HOME/.ssh/authorized_keys
