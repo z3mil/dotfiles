1d92ddef1098b945378998c4ee25056868d6aca9
z3mil <72978258+z3mil@users.noreply.github.com>
2023-07-22 12:59:13 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@
 SHELLRC="$HOME/.bashrc"
 PS1="\[\033[m\]|\[\033[1;35m\]\t\[\033[m\]|\[\e[1;31m\]\u\[\e[1;36m\]\[\033[m\]@\[\e[1;36m\]\h\[\033[m\]:\[\e[0m\]\[\e[1;32m\][\W]> \[\e[0m\]"
 USER=`whoami`
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
 
 # Help function
 helpFunction()
@@ -20,13 +23,11 @@ helpFunction()
 
 # Check if we are running as non-root user
 if [[ $EUID -eq 0 ]]; then
-  echo "WARNING: This script is run as root - SSH access as root is not allowed, skipping public key step."
+  echo "INFO: This script is run as root."
   IAMROOT=true
 else
-  echo "INFO: This script is run as $USER."
+  echo "INFO: This script is run as $USER. run as root to set Hostname."
   IAMROOT=false
-  # helpFunction
-  # exit 1
 fi
 
 # Check for correct number of arguments
@@ -39,9 +40,6 @@ do
     ? ) helpFunction ;;
   esac
 done
-
-# Set hostname
-hostnamectl set-hostname "$HOSTNAME"
 
 # Set PS1 variable
 echo "PS1='$PS1'" >> $SHELLRC
@@ -56,5 +54,11 @@ if [ ! -d $HOME/.ssh ]; then
   if [ $IAMROOT = false ]; then
      echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGVvVrgF7abC0Bk8KIeNLfTT+wGvHPodJkt0YkS04eNF" >> $HOME/.ssh/authorized_keys
      chmod 600 $HOME/.ssh/authorized_keys
+  else
+     echo "INFO: SSH access as root is not allowed, skipping public key step.."
+     # Set hostname
+     echo "INFO: Setting HOSTNAME.."
+     hostnamectl set-hostname "$HOSTNAME"
   fi
 fi
+echo "${GREEN}SUCCESS: Script executed successfully.${NC}"
